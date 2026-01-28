d7c8cbcaab71c408a34a693e64551ce7c47d5354
Dhruv10633 <dhruvbisht1265@gmail.com>
2026-01-28 15:18:10 +0530

    Initial setup

---
+++ src/main/java/com/trashview/backend/TrashviewBackendApplication.java
@@ -0,0 +1,14 @@
+package com.trashview.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Entry Point
+@SpringBootApplication
+public class TrashviewBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrashviewBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trashview/backend/TrashviewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trashview.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrashviewBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
