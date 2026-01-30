8a16955449a2b7c4b1075186216405227833e671
Dhruv10633 <dhruvbisht1265@gmail.com>
2026-01-30 16:00:29 +0530

    Controller modified

--- src/main/java/com/trashview/backend/controllers/WelcomeController.java
+++ src/main/java/com/trashview/backend/controllers/WelcomeController.java
@@ -1,14 +1,18 @@
 package com.trashview.backend.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller responsible for sending response when we enter URL
 @RestController
-public class HelloController {
+public class WelcomeController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello World";
+    @Value("${app.welcome.message}")
+    private String welcomeMessage;
+
+    @GetMapping
+    public String getWelcomeMessage() {
+        return welcomeMessage;
     }
 }
